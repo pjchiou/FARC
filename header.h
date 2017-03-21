a6ab546116fe8242749232c3fe3ac11dd013b53d
CPJ <pjchiou0418@gmail.com>
2017-03-21 09:46:00 +0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,1 @@
+#ifndef __header_h__#define __header_h__#include <iostream>#include <vector>#include <string>#include <cstdlib>using namespace std;class cReport{private: string sName,sPath; vector<double> vVersion;public: string Name(){return(sName);} string Path(){return(sPath);}};#endif
