8b39bcd3946da5359a501b74f27c8cf26f88a808
CPJ <pjchiou0418@gmail.com>
2017-03-21 17:41:26 +0800

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,22 @@
-#ifndef __header_h__#define __header_h__#include <iostream>#include <vector>#include <string>#include <cstdlib>using namespace std;class cReport{private: string sName,sPath; vector<double> vVersion;public: string Name(){return(sName);} string Path(){return(sPath);}};#endif
+#ifndef __header_h__
+#define __header_h__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
+
+class cReport
+{
+private:
+    string sName,sPath;
+    vector<double> vVersion;
+public:
+    string Name(){return(sName);}
+    string Path(){return(sPath);}
+};
+
+#endif
